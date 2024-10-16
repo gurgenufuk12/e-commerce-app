b2150a20d6e3506fd7f1df026a28993fed2cf45a
gurgenufuk12 <doguhangurgen8@gmail.com>
2024-10-17 00:15:21 +0300

    for now just add prodcut

---
+++ frontend/src/pages/AdminDashboard.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ProductForm from "../components/ProductForm.tsx";
+
+const AdminDashboard = () => {
+  return (
+    <div>
+      <h1>Admin Dashboard</h1>
+      <ProductForm />
+    </div>
+  );
+};
+
+export default AdminDashboard;
