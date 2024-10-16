822fb95bdce9c16b7cffd124e4bfc67394524e3a
gurgenufuk12 <doguhangurgen8@gmail.com>
2024-10-17 00:42:43 +0300

    implemet if useful for each type of is

--- frontend/src/hooks/useRandomStringGenerator.tsx
+++ frontend/src/hooks/useRandomStringGenerator.tsx
@@ -1,12 +1,13 @@
+import { stringify } from "querystring";
 import { useCallback } from "react";
 
 const useRandomStringGenerator = () => {
-  const generateRandomString = useCallback(() => {
+  const generateRandomString = useCallback((type: string) => {
     const min = 1000000000000000;
     const max = 9999999999999999;
     const randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
 
-    return "P" + randomNumber.toString();
+    return `${type}-${randomNumber}`;
   }, []);
 
   return { generateRandomString };
