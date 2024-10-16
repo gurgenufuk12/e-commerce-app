caeec17992dfc1e6ba4b6b4e642f4d85086c2e16
gurgenufuk12 <doguhangurgen8@gmail.com>
2024-10-17 00:13:27 +0300

    routes added

--- backend/index.js
+++ backend/index.js
@@ -5,10 +5,14 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 const config = require("./config");
 
-const PORT = 8000;
+const userRoutes = require("./routes/userRoutes");
+const productRoutes = require("./routes/productRoutes");
 app.use(express.json());
 app.use(bodyParser.json());
 app.use(cors());
+
+app.use("/api/user", userRoutes);
+app.use("/api/product", productRoutes);
 
 app.listen(config.port, () => {
   console.log("Server is running on http://localhost:" + config.port);
