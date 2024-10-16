2657c0fd5aba0f499deae89e05662b17747cb643
gurgenufuk12 <doguhangurgen8@gmail.com>
2024-10-17 00:43:07 +0300

    add Category form to Admin Dasboard

--- frontend/src/pages/AdminDashboard.tsx
+++ frontend/src/pages/AdminDashboard.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import ProductForm from "../components/ProductForm.tsx";
+import CategoryForm from "../components/CategoryForm.tsx";
 
 const AdminDashboard = () => {
   return (
     <div>
       <h1>Admin Dashboard</h1>
       <ProductForm />
+      <CategoryForm />
     </div>
   );
 };
