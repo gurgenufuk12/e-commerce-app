e47eb3d7d3418a01db17ce8cfbf126f5f3cd6f0b
gurgenufuk12 <doguhangurgen8@gmail.com>
2024-10-17 00:14:25 +0300

    Admin Dashboard page implemented

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,3 @@
-// src/App.tsx
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from "./components/NavBar.tsx";
@@ -7,6 +6,7 @@ import Login from "./pages/Login.tsx";
 import Register from "./pages/Register.tsx";
 import { AuthProvider } from "./contexts/AuthContext.tsx";
 import ProductDetail from "./pages/ProductDetail.tsx";
+import AdminDashboard from "./pages/AdminDashboard.tsx";
 
 const App = () => {
   return (
@@ -18,6 +18,7 @@ const App = () => {
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
           <Route path="/product/:id" element={<ProductDetail />} />
+          <Route path="/admin" element={<AdminDashboard />} />
         </Routes>
       </Router>
     </AuthProvider>
