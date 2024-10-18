089a313604729c49ded7c3709b2a7f57c7b95c9e
gurgenufuk12 <doguhangurgen8@gmail.com>
2024-10-18 09:29:35 +0300

    add more attributes for Product

--- frontend/src/components/ProductCard.tsx
+++ frontend/src/components/ProductCard.tsx
@@ -3,20 +3,47 @@ import { Link } from "react-router-dom";
 
 interface ProductCardProps {
   id: string;
+  brand: string;
   name: string;
   price: number;
   description: string;
+  color: string;
+  stock: number;
+  categoryId: string;
+  categoryName: string;
 }
 
-const ProductCard: React.FC<ProductCardProps> = ({ id, name, price, description }) => {
+const ProductCard: React.FC<ProductCardProps> = ({
+  id,
+  brand,
+  name,
+  price,
+  description,
+  color,
+  stock,
+  categoryId,
+  categoryName,
+}) => {
   return (
     <Link
       to={`/product/${id}`}
-      state={{ id, name, price, description }}
+      state={{
+        id,
+        name,
+        price,
+        description,
+        color,
+        stock,
+        categoryId,
+        categoryName,
+      }}
       className="p-4 border rounded-lg shadow-lg hover:shadow-xl transition-shadow duration-200 w-64 h-64 flex flex-col items-center justify-between"
     >
       <div className="flex-grow flex items-center justify-center">
-        <h3 className="text-lg font-semibold text-center">{name}</h3>
+        <h3 className="text-lg font-semibold text-center">
+          {brand}
+          {name}
+        </h3>
       </div>
       <p className="text-gray-700">Price: {price}</p>
       <button className="mt-2 bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 transition-colors duration-200">
