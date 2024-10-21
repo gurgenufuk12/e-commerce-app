807ad8b6b3d186bcbb438773ab2c7890424793c0
gurgenufuk12 <doguhangurgen8@gmail.com>
2024-10-21 16:33:06 +0300

    Button to add product to Cart

--- frontend/src/pages/ProductDetail.tsx
+++ frontend/src/pages/ProductDetail.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 import { useLocation } from "react-router-dom";
+import AddToCartButton from "../components/AddToCartButton.tsx";
 
 const ProductDetail = () => {
   const location = useLocation();
   const product = location.state;
-
-  console.log("Product Details: ", product);
+  const [quantity, setQuantity] = React.useState(1);
 
   if (!product) {
     return <div>Product not found.</div>;
@@ -14,14 +14,31 @@ const ProductDetail = () => {
   return (
     <div className="mx-24 mt-8">
       <h1 className="text-2xl font-bold">{product.name}</h1>
-      <p className="text-gray-700">Price: {product.price}</p>
-      <p className="text-gray-700">Stock: {product.stock}</p>
-      <p className="text-gray-700">Category: {product.categoryName}</p>
-      <p className="text-gray-700">Color: {product.color}</p>
-      <p className="mt-4">{product.description}</p>
-      <button className="mt-4 bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 transition-colors duration-200">
-        Add to Cart
-      </button>
+      <p className="text-gray-700 font-bold">Price: {product.price}</p>
+      {product.stock > 0 ? (
+        <p className="text-green-500">In Stock</p>
+      ) : (
+        <p className="text-red-500">Out of Stock</p>
+      )}
+      <p className="text-gray-700 font-bold">
+        Category: {product.categoryName}
+      </p>
+      <p className="text-gray-700 font-bold">Color: {product.color}</p>
+      <p className="mt-4 text-gray-700 font-bold">
+        {" "}
+        Descrition: {product.description}
+      </p>
+      <input
+        type="number"
+        value={quantity}
+        onChange={(e) => setQuantity(parseInt(e.target.value))}
+        className="border border-gray-300 rounded-lg px-2 py-1 mt-4"
+      />
+      <AddToCartButton
+        product={product}
+        quantity={quantity}
+        disabled={product.stock === 0}
+      />
     </div>
   );
 };
