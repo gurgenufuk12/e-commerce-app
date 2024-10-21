90752efb499619c46066c47b0a2a6e07b4b4e4e4
gurgenufuk12 <doguhangurgen8@gmail.com>
2024-10-21 16:33:27 +0300

    Add CategoryBar for HOme page

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,7 +1,8 @@
 import React, { useContext, useState, useEffect } from "react";
-import { getAllProducts } from "../services/api.ts";
+import { getAllProducts, getAllCategories } from "../services/api.ts";
 import { AuthContext } from "../contexts/AuthContext.tsx";
 import ProductCard from "../components/ProductCard.tsx";
+import CatergoryBar from "../components/CategoryBar.tsx";
 
 interface Product {
   productId: string;
@@ -17,42 +18,23 @@ interface Product {
 const Home = () => {
   const authContext = useContext(AuthContext);
   const [products, setProducts] = useState<Product[]>([]);
-  useEffect(() => {
-    const fetchProducts = async () => {
-      try {
-        const productList = await getAllProducts();
-        setProducts(productList);
-      } catch (error) {
-        console.error("Failed to fetch products:", error);
-      }
-    };
 
+  const fetchProducts = async () => {
+    try {
+      const productList = await getAllProducts();
+      setProducts(productList);
+    } catch (error) {
+      console.error("Failed to fetch products:", error);
+    }
+  };
+
+  useEffect(() => {
     fetchProducts();
   }, []);
 
   return (
     <div className="mx-24">
-      <h1 className="text-red-700 text-2xl font-bold">
-        Welcome to Our E-Commerce Store
-      </h1>
-      {authContext?.user ? (
-        <p className="mt-4">
-          Hello, {authContext.user.email}! Explore our products below:
-        </p>
-      ) : (
-        <p className="mt-4">
-          Welcome, guest! Please{" "}
-          <a href="/login" className="text-blue-500 underline">
-            login
-          </a>{" "}
-          or{" "}
-          <a href="/register" className="text-blue-500 underline">
-            register
-          </a>{" "}
-          to enjoy a personalized shopping experience.
-        </p>
-      )}
-
+      <CatergoryBar />
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6 mt-8">
         {products.map((product) => (
           <ProductCard
