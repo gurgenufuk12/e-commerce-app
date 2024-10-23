df78946da85d8045081adb7945cf0eca2bbb7aa6
gurgenufuk12 <doguhangurgen8@gmail.com>
2024-10-23 09:37:17 +0300

    update Product type

--- frontend/src/types/Product.ts
+++ frontend/src/types/Product.ts
@@ -2,8 +2,14 @@ export interface Product {
   id: string;
   name: string;
   price: number;
+  description?: string;
   color: string;
   quantity: number;
   stock: number;
   totalPrice?: number;
+  categoryId?: string;
+  categoryName?: string;
+  generalCategory?: string;
+  brand?: string;
+  comments?: string[];
 }
